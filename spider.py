02e843749d036515251b605af96f6ec531afdb5c
uppcat <bigmaxgod@qq.com>
2019-06-05 11:13:43 +0800

    spider1

--- spider.py
+++ spider.py
@@ -3,14 +3,14 @@ import json
 res = requests.session()
 def spider(url):
     response = res.get(url=url)
-    print(response.json())
+    print(response.text)
 
 
 import gevent
 gevent.joinall([
-    gevent.spawn(spider(http://hq.sinajs.cn/list=sh601002))
-    gevent.spawn(spider(http://hq.sinajs.cn/list=sh601003))
-    gevent.spawn(spider(http://hq.sinajs.cn/list=sh601004))
-    gevent.spawn(spider(http://hq.sinajs.cn/list=sh601005))
-    gevent.spawn(spider(http://hq.sinajs.cn/list=sh601006))
+    gevent.spawn(spider("http://hq.sinajs.cn/list=sh601002")),
+    gevent.spawn(spider("http://hq.sinajs.cn/list=sh601003")),
+    gevent.spawn(spider("http://hq.sinajs.cn/list=sh601004")),
+    gevent.spawn(spider("http://hq.sinajs.cn/list=sh601005")),
+    gevent.spawn(spider("http://hq.sinajs.cn/list=sh601006"))
 ])
